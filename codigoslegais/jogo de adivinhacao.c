2e2af5e03a869e544c90399528180e27e1e9254a
Lucas Chang <chekai.work155@gmail.com>
2026-04-28 12:13:12 -0300

    organizei a pasta

---
+++ codigoslegais/jogo de adivinhacao.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+srand(time(NULL));
+int num = (rand() % 100) + 1;
+int chut;
+printf("******************************vamos jogar um jogo de adivinhação******************************\n");
+while(1){
+printf("digite um numero:\n");
+scanf("%d",&chut);
+if(chut==num) {
+printf("voce acertou o numero!\n");
+break;
+}else if(chut<num){
+printf("muito baixo, digite outro numero:\n");
+}else if(chut>num){
+printf("muito alto, digite outro nu\n");
+}
+}
+return 0;
+}
