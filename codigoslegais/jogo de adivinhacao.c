171e9bbe39f4631f16c8b1945c9f93deea41e0ae
Lucas Chang <chekai.work155@gmail.com>
2026-05-05 21:36:06 -0300

    update the jogo

--- codigoslegais/jogo de adivinhacao.c
+++ codigoslegais/jogo de adivinhacao.c
@@ -4,19 +4,23 @@
 int main(){
 srand(time(NULL));
 int num = (rand() % 100) + 1;
-int chut;
+int chut, cont=0, menor=0,maior=100;
 printf("******************************vamos jogar um jogo de adivinhação******************************\n");
 while(1){
 printf("digite um numero:\n");
 scanf("%d",&chut);
+cont++;
 if(chut==num) {
-printf("voce acertou o numero!\n");
+printf("voce acertou o numero!, a quantidade de chute foi:%d\n", cont);
 break;
 }else if(chut<num){
-printf("muito baixo, digite outro numero:\n");
+    menor=chut;
+printf("muito baixo, digite outro numero de %d a %d:\n", menor, maior);
 }else if(chut>num){
-printf("muito alto, digite outro nu\n");
+    maior=chut;
+printf("muito alto, digite outro numero de %d a %d:\n", menor, maior);
 }
 }
+
 return 0;
 }
